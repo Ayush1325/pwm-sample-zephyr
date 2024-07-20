3511e8ac6015a357c55798ad7d7e4cc429cba394
Ayush Singh <ayush@beagleboard.org>
2024-07-20 16:54:06 +0530

    Make things easier to test
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- src/main.c
+++ src/main.c
@@ -14,21 +14,30 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 
+#define DELAY 30
+
 static const struct pwm_dt_spec pwm_led0 = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led0));
 int step = 5;
+
+static const uint32_t start = 0;
+static const uint32_t period = pwm_led0.period;
 
 int main(void) {
   int i;
 
   while (1) {
-    for (i = 0; i < pwm_led0.period; i += step) {
+    // pwm_set_pulse_dt(&pwm_led0, 0);
+    // k_sleep(K_MSEC(DELAY));
+    for (i = start; i < period; i += step) {
       pwm_set_pulse_dt(&pwm_led0, i);
-      k_sleep(K_MSEC(30));
+      // k_busy_wait(30000);
+      k_sleep(K_MSEC(DELAY));
     }
 
-    for (i = pwm_led0.period; i > 0; i -= step) {
+    for (i = period; i > start; i -= step) {
       pwm_set_pulse_dt(&pwm_led0, i);
-      k_sleep(K_MSEC(30));
+      // k_busy_wait(30000);
+      k_sleep(K_MSEC(DELAY));
     }
   }
 
